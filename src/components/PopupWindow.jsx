fa9e5d1dcbae96edb3f78d1e9718822312533c4a
merlinymy <lordhorcrux@gmail.com>
2025-05-03 13:06:56 -0500

    fix sound

--- src/components/PopupWindow.jsx
+++ src/components/PopupWindow.jsx
@@ -23,10 +23,7 @@ export function PopupWindow({
   gameInfo = { endlessScore: 0 },
 }) {
   useEffect(() => {
-    // AudioManager.stopBgm();
-    // setTimeout(() => {
     AudioManager.playBgm("battleResult");
-    // }, 3000);
   });
 
   const resetClicks = () => setClickedCards([]);
